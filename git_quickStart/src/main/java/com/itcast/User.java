b6b1f6e3e5f8d824b3aeec67a09791f98d1a626d
itcast <bos@itcast.cn>
2019-05-12 21:55:49 +0800

    创建User.java,提交到版本可以

---
+++ git_quickStart/src/main/java/com/itcast/User.java
@@ -0,0 +1,12 @@
+package com.itcast;
+
+/**
+ * @program: git_quickStart
+ * @description
+ * @author: allen
+ * @params
+ * @create: 2019-05-12 21:53
+ **/
+public class User {
+    private String username;
+}
